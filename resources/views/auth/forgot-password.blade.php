171fed2354677e078f8cbea2d7991348773469c6
deavitaawandae <77710952+deavitaawandae@users.noreply.github.com>
2021-03-15 14:24:37 +0700

    Add files via upload

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,34 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600">
+            {{ __('Lupa Sandi? Bukan Masalah. Masukkan email Anda dan saya akan mengirimkan link pemulihan sandi.') }}
+        </div>
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <x-jet-validation-errors class="mb-4" />
+
+        <form method="POST" action="/forgot-password">
+            @csrf
+
+            <div class="block">
+                <x-jet-label value="Email" />
+                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-jet-button>
+                    {{ __('Reset Sandi') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,37 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600">
+            {{ __('Terimakasih Telah Mendaftar! Sebelum memulai, dapatkan anda memferivikasi alamat email kamu dengan memencet link di email kamu? jika kamu tidak menerima email, kami akan mengirimkannya lagi.') }}
+        </div>
+
+        @if (session('status') == 'verification-link-sent')
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ __('Link verivikasi akan dikirimkan ke akun anda.') }}
+            </div>
+        @endif
+
+        <div class="mt-4 flex items-center justify-between">
+            <form method="POST" action="/email/verification-notification">
+                @csrf
+
+                <div>
+                    <x-jet-button type="submit">
+                        {{ __('Kirim Ulang Verification Email') }}
+                    </x-jet-button>
+                </div>
+            </form>
+
+            <form method="POST" action="/logout">
+                @csrf
+
+                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    {{ __('Logout') }}
+                </button>
+            </form>
+        </div>
+    </x-jet-authentication-card>
+</x-guest-layout>
