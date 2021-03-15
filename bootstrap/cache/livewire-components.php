b9e8ee19380126338698e2ebb685cff194475bcf
deavitaawandae <77710952+deavitaawandae@users.noreply.github.com>
2021-03-15 14:10:47 +0700

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'content' => 'App\\Http\\Livewire\\Content',
+  'footer' => 'App\\Http\\Livewire\\Footer',
+  'my-recipe' => 'App\\Http\\Livewire\\MyRecipe',
+  'navbar' => 'App\\Http\\Livewire\\Navbar',
+  'profile' => 'App\\Http\\Livewire\\Profile',
+  'recipe-categorized' => 'App\\Http\\Livewire\\RecipeCategorized',
+  'recipe-index' => 'App\\Http\\Livewire\\RecipeIndex',
+  'recipe-list' => 'App\\Http\\Livewire\\RecipeList',
+  'search' => 'App\\Http\\Livewire\\Search',
+  'store-recipe' => 'App\\Http\\Livewire\\StoreRecipe',
+  'subscribe' => 'App\\Http\\Livewire\\Subscribe',
+);
